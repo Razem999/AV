4983c29940a44d14282ecd32adc77e92fc4759ed
Razem999 <razem999@gmail.com>
2023-02-23 16:43:27 -0500

    Test CAN Message Transmission

--- ECU_scripts/CAN_read/CAN_read.ino
+++ ECU_scripts/CAN_read/CAN_read.ino
@@ -9,14 +9,7 @@ MCP2515 mcp2515(10);
 
 
 void setup() {
-  Serial.begin(500000);
-
-  // Initialize LED
-  pinMode(LED0, OUTPUT);
-  pinMode(LED1, OUTPUT);
-
-  digitalWrite(LED1, HIGH);
-  digitalWrite(LED0, HIGH);
+  Serial.begin(9600);
   
   mcp2515.reset();
   mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
@@ -37,33 +30,6 @@ void loop() {
       Serial.print(canMsg.data[i]);
       Serial.print(" ");
     }
-
-    // digitalWrite(LED0, LOW);
-    // delay(1000);
-    // digitalWrite(LED0, HIGH);
-    // delay(1000);
-
-    // digitalWrite(LED1, LOW);
-    // delay(1000);
-    // digitalWrite(LED1, HIGH);
-    // delay(1000);
-
-    if (canMsg.data[0] == 0) {
-      digitalWrite(LED0, LOW);
-      digitalWrite(LED1, HIGH);
-    }
-    else {
-      digitalWrite(LED0, HIGH);
-      digitalWrite(LED1, LOW);
-    }
-
     Serial.println();
   }
-
-  // if (canMsg.data[0] == 0) {
-  //   digitalWrite(LED, LOW);
-  // }
-  // else {
-  //   digitalWrite(LED, HIGH);
-  // }
 }
