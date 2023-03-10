e73abd953cbccc0010b112addec5b2775b45c02b
Atteh08 <77075788+Atteh08@users.noreply.github.com>
2023-03-10 16:51:46 -0500

    Update data_dashboard.py
    Should be able to get data directly from the dashboard and display it

--- Sub-systems/data_dashboard.py
+++ Sub-systems/data_dashboard.py
@@ -1,50 +1,64 @@
-#using pyqt5 on jetson try and install this with it's drivers
-
-
-# There are several GUI toolkits available in Python such as PyQt, Tkinter, wxPython, etc. You can choose the one that you are most familiar with or that best suits your needs.
-
-# Install the required libraries: Depending on the toolkit you choose, you may need to install additional libraries. For example, if you choose PyQt, you will need to install PyQt5 library. You can install these libraries using pip or your package manager.
-
-# Design the layout of the dashboard: Once you have chosen the GUI toolkit and installed the necessary libraries, you can start designing the layout of the dashboard. You can use tools like Qt Designer to create the layout of the dashboard and save it as a .ui file.
-
-# Convert the .ui file to Python code: You can use the pyuic5 tool to convert the .ui file to Python code. This code will contain the definition of the UI widgets and their properties.
-
-# Write the code for the dashboard: Using the generated Python code, you can add the logic to the dashboard. For example, you can add functionality to update the speedometer or fuel gauge based on the car's data.
-
-# Test the dashboard: You can run the Python script and test the dashboard in your Linux environment.
-
-# Here is a sample code snippet that demonstrates how to create a basic car dashboard using PyQt:
-
-# python
-
-import Jetson.GPIO as GPIO
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.uic import loadUi
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QApplication, QGridLayout, QLabel, QMainWindow, QWidget
 
 class Dashboard(QMainWindow):
     def __init__(self):
-        super(Dashboard, self).__init__()
-        loadUi('dashboard.ui', self)
-        self.show()
+        super().__init__()
+
+        # Initialize the CAN bus and set up message filters
+        self.bus = can.interface.Bus(bustype='socketcan', channel='can0')
+        self.bus.set_filters([
+            {"can_id": 0x100, "can_mask": 0x7FF},
+            {"can_id": 0x101, "can_mask": 0x7FF},
+            {"can_id": 0x102, "can_mask": 0x7FF},
+        ])
+
+        # Create the widgets for the dashboard
+        self.mode_label = QLabel("Mode: N/A")
+        self.mode_label.setAlignment(Qt.AlignCenter)
+        self.mode_label.setFont(QFont("Arial", 18))
+        
+        self.speed_label = QLabel("Speed: N/A")
+        self.speed_label.setAlignment(Qt.AlignCenter)
+        self.speed_label.setFont(QFont("Arial", 18))
+        
+        self.object_label = QLabel("Object Detected: N/A")
+        self.object_label.setAlignment(Qt.AlignCenter)
+        self.object_label.setFont(QFont("Arial", 18))
+
+        # Create a grid layout for the widgets
+        grid = QGridLayout()
+        grid.addWidget(self.mode_label, 0, 0)
+        grid.addWidget(self.speed_label, 0, 1)
+        grid.addWidget(self.object_label, 0, 2)
+
+        # Create a central widget and set the grid layout as its layout
+        central_widget = QWidget()
+        central_widget.setLayout(grid)
+        self.setCentralWidget(central_widget)
+
+        # Set up a timer to read data from the CAN bus every 100ms
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_data)
+        self.timer.start(100)
+
+    def update_data(self):
+        # Read the data from the CAN bus
+        messages = self.bus.recv(10)
+        for message in messages:
+            if message.arbitration_id == 0x100:
+                self.mode_label.setText(f"Mode: {message.data[0]}")
+            elif message.arbitration_id == 0x101:
+                speed = (message.data[0] << 8) | message.data[1]
+                self.speed_label.setText(f"Speed: {speed} km/h")
+            elif message.arbitration_id == 0x102:
+                object_detected = message.data[0]
+                self.object_label.setText(f"Object Detected: {object_detected}")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     dashboard = Dashboard()
+    dashboard.show()
     sys.exit(app.exec_())
-
-
-
-# Set up GPIO pins for the Jetson's speed sensor
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.IN)
-
-# Loop to continuously read the speed sensor and display speed on the dashboard
-while True:
-    speed = GPIO.input(12)
-    # Display the speed on the dashboard
-
-
-
-
-
