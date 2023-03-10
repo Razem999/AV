c71ce4ce28cc24cea7eb62106b8f6becb27690c1
Razem Muhammad Shahin <razemshahin@cmail.carleton.ca>
2023-03-10 00:12:52 -0500

    Add initialization settings

--- Sub-systems/Automation.py
+++ Sub-systems/Automation.py
@@ -1,12 +1,14 @@
-
+# Assuming speed limit of this road is 80km/h
 # Vehicle class keeps track of the vehicle's state
 class vehicle:
-    def __init__(self, speed, x_position, y_position, x_steering, y_steering):
-        self.speed: 0
-        self.x_position: 0
-        self.y_position: 0
-        self.x_steering: 0
-        self.y_steering: 0
+    def __init__(self, speed, x_position, y_position, x_steering, sensor_range, mode):
+        self.speed: speed                   # Vehicle Speed
+        self.x_position: x_position         # Vehicle Position (x)
+        self.y_position: y_position         # Vehicle Position (y)
+        self.x_steering: x_steering         # Vehicle Steering Angle (x)
+        self.sensor_range: sensor_range     # Vehicle Sensor Range
+        # Vehicle in Manual (0) or Automatic (1)
+        self.mode: mode
 
 
 # Object class keeps track of the object's state
@@ -17,23 +19,27 @@ class object:
         self.x_position: 0
         self.y_position: 0
 
-# Assuming speed limit of this road is 80km/h
+
+# Main Automation method
+def automation():
+    # Initialize Automated Vehicle (Tabby) settings
+    speed = 60
+    x_position = 0
+    y_position = 0
+    x_steering = 0
+    sensor_range = 150
+    mode = 1
+    tabby = vehicle(speed, x_position, y_position,
+                    x_steering, sensor_range, mode)
+
+    while(tabby.mode == 1):
+        checkSurrounding()
+
+
 # Instructions are prioritized here
 
 
 def prioritizeInstructions():
-    return False
-
-# Initialize Automated Vehicle Settings
-
-
-def initialize():
-    # Vehicle Speed
-
-    # Vehicle Position (x, y)
-
-    # Vehicle Steering Angle (x)
-
     return False
 
 # Check Surroundings (This will be looping)
@@ -89,3 +95,7 @@ def emergencyBrakes():
 
 def calculateLeadingSpeed(vspeed, vxposition, vyposition):
     return False
+
+
+initialize()
+print(vehicle.speed)
