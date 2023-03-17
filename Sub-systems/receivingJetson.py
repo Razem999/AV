006132adb176684462060977ff3bd5597777aba0
Atteh08 <77075788+Atteh08@users.noreply.github.com>
2023-03-17 02:08:51 -0400

    Update receivingJetson.py
    create a text file and put all the can messages in that file

--- Sub-systems/receivingJetson.py
+++ Sub-systems/receivingJetson.py
@@ -1,12 +1,44 @@
 import can
+import datetime
 
 # Create a CAN bus instance using the socketcan backend
-bus = can.interface.Bus(channel='can0', bustype='socketcan')
+bus = can.Bus(channel='can0', bustype='socketcan')
 
 # Create an empty list to store the CAN messages
 can_dump = []
 
 # Read in the CAN messages and store them in the can_dump list
 while True:
-    message = bus.recv()
-    can_dump.append(message)
+    message = bus.recv(1.0)
+    message_dict ={
+        'id': hex(message.arbitration_id),
+        'data':message.data,
+        "timestamp": datetime.datetime.fromtimestamp(message.timestamp),
+        'dlc(datalength)': message.dlc
+    }
+    can_dump.append(message_dict)
+    print(hex(message_dict['data'][0]))
+    #print(hex(message_dict['id']))
+    #print(message_dict['timestamp'])
+    print(message_dict['id'])
+
+    #print(f"ID:{message_dict['id']} TimeStamp:{message_dict['timestamp']}DLC:{message_dict['dlc']} Data:{message_dict['data'].hex()}")
+    #print(message)
+    # Open the output file for writing
+    output_file = open('can_messages.txt', 'w')
+    # Define a message listener function
+    def message_listener(msg):
+        # Write the message to the output file
+        output_file.write(str(msg) + '\n')
+    # Wait for messages
+    while True:
+        pass  # This loop will run forever, until the program is interrupted or stopped manually
+    # Add the message listener to the bus
+    notifier = can.Notifier(bus, [message_listener])
+        
+        if message is None:
+            print('Timeout occured, no message.')
+
+
+
+ 
